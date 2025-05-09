678f2ea24cb1947b041dc92ec4174bc1d29cac05
Sebastian Widelak <sebastian.widelak@justpremium.com>
2025-05-09 09:55:52 +0200

    feat: 2.0.7

--- dist/react-flag.js
+++ dist/react-flag.js
@@ -1,19 +1,20 @@
 import { jsx as e } from "react/jsx-runtime";
-const d = ({
-  code: a = "NL",
-  size: r = "l",
+const i = ({
+  code: r = "NL",
+  size: s = "l",
   gradient: l = "",
-  hasBorder: o = !0,
-  hasDropShadow: t = !1,
-  hasBorderRadius: $ = !0,
-  className: s
+  hasBorder: t = !0,
+  hasDropShadow: $ = !1,
+  hasBorderRadius: d = !0,
+  alt: o = "",
+  className: a
 }) => /* @__PURE__ */ e(
   "div",
   {
-    className: `flag ${l} size-${r} ${o ? "border" : ""} ${t ? "drop-shadow" : ""} ${$ ? "border-radius" : ""} ${s ? s.replace(/\s\s+/g, " ").trim() : ""}`,
-    children: /* @__PURE__ */ e("img", { src: `/flags/${r}/${a}.svg` })
+    className: `flag ${l} size-${s} ${t ? "border" : ""} ${$ ? "drop-shadow" : ""} ${d ? "border-radius" : ""} ${a ? a.replace(/\s\s+/g, " ").trim() : ""}`,
+    children: /* @__PURE__ */ e("img", { alt: o ?? r, src: `/flags/${s}/${r}.svg` })
   }
-), i = d;
+);
 export {
   i as default
 };
